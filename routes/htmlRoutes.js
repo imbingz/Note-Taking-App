83d15140aa8c7d2e4e42cbf2af223d42a78cc47a
Bing Z <bing@bings-mbp.lan>
2020-10-05 16:59:02 -0600

    set html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -7,4 +7,25 @@ The following HTML routes should be created:
 */
 
 
-// export the file 
+
+// IMPORT MODULES ================================================================================
+
+const path = require('path');
+
+
+
+//SET UP HTML ROUTES AND EXPORT THE MODULE ================================================================================
+
+module.exports = function(app) {
+
+  //Define GET `/notes` request route- Should return the `notes.html` file
+  app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/notes.html"))
+  })
+
+  //Set default route to home for all other page  request - should return the `index.html` file
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+  })
+
+}
