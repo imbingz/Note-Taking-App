a7dc834782ea289f8142d89c324227fadf861484
Bing Z <bing@bings-mbp.lan>
2020-10-05 13:09:01 -0600

    set up the server

--- server.js
+++ server.js
@@ -2,3 +2,30 @@
 
 
 // IMPORT MODULES AND FILES ================================================================================
+
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express(); 
+
+// require("./routes/htmlRoutes")(app);
+// require("./routes/apiRoutes")(app);
+
+
+// SET AN INITIAL PORT ======================================================================================
+
+let PORT = process.env.PORT || 8080;
+
+// SET UP MIDDLEWARES TO HANDLE DATA PARSIING ======================================================================================
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); 
+app.use(express.static('public'))
+
+
+// SET UP LISTEN EVENT ON THE SERVER ======================================================================================
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT", PORT );
+})
