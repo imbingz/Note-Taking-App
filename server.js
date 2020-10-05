a32a9ee9c1e80875ef98b33c98a7bc860001763e
Bing Z <bing@bings-mbp.lan>
2020-10-05 12:54:49 -0600

    moved all files out of Dev dir, del Dev dir

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 //The application should have a db.json file on the backend that will be used to store and retrieve notes using the fs module.
 
 
-// import fs 
+// IMPORT MODULES AND FILES ================================================================================
