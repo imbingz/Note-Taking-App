bbd53d515dff71755249f0246246085687129f7c
Bing Z <bing@bings-mbp.lan>
2020-10-05 17:03:19 -0600

    set get api/note router

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,3 +7,27 @@ POST /api/notes - Should receive a new note to save on the request body, add it 
 
 DELETE /api/notes/:id - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique id when it's saved. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
 */
+
+
+
+// IMPORT MODULES AND FILES ================================================================================
+
+const fs = require('fs');
+
+const db = require('../db/db.json')
+
+//SET UP API ROUTES AND EXPORT THE MODULE ================================================================================
+
+module.exports = function(app) {
+
+  //API GET request 
+  app.get('/api/notes', (req, res) => {
+    //Read json data file 
+    fs.readFile('../db/db.json', "utf-8", (err, data) => {
+      if (err) throw err;
+    //Send data json file
+      res.json(data)
+    })
+  })
+
+}
