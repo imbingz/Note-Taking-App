9b3366f7f5ca6221555497a9a2b99be0d2bc162e
Bing Z <bing@bings-mbp.lan>
2020-10-05 20:33:28 -0600

    added check title/text before adding id

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,70 +8,69 @@ POST /api/notes - Should receive a new note to save on the request body, add it 
 DELETE /api/notes/:id - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique id when it's saved. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
 */
 
-
-
 // IMPORT MODULES AND FILES ================================================================================
 
 const fs = require('fs');
 
+const db = require('../db/db.json');
+
 //SET UP API ROUTES AND EXPORT THE MODULE ================================================================================
+console.log("line 18 db:", db);
 
 module.exports = function(app) {
+	//API GET request
+	app.get('/api/notes', (req, res) => {
+		res.json(db);
+	});
 
-  //API GET request 
-  app.get('/api/notes', (req, res) => {
-    //Read json data file 
-    fs.readFile('../db/db.json', "utf-8", (err, data) => {
-      if (err) throw err;
-    //Send data json file
-      res.json(data)
-    })
-  })
+	//API POST request
+	app.post('/api/notes', (req, res) => {
+		//Read json data file
+		fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+			if (err) throw err;
 
-  //API POST request 
-  app.post('/api/notes', (req, res) => {
-    //Read json data file 
-    fs.readFile('../db/db.json', "utf-8", (err, data) => {
-      if (err) throw err;
-    
-      const notes = JSON.parse(data);
+			const notes = JSON.parse(data);
 
-      //Add id prop to each new note saved
-      req.body["id"] = notes.length + 1;
+			//Add id prop to each new note saved
+			if (req.body.title && req.body.text) {
+				req.body['id'] = notes.length + 1;
+				//Add new note to parsed data
+				notes.push(req.body);
+      } else {
+        console.log("Enter note title and text before saving the new note.");
+      }
 
-      //Add new note to parsed data
-      notes.push(req.body);
+      console.log("line 40 req.body:", req.body);
 
-      //Write the updated notes to db.json
-      fs.writeFile('../db/db.json', JSON.stringify(notes, null, '\t'), err => {
-        if (err) throw err;
-      })
+			//Write the updated notes to db.json
+			fs.writeFile('./db/db.json', JSON.stringify(notes, null, '\t'), (err) => {
+				if (err) throw err;
 
-      //Send new note to client
-      res.json(req.body);
-    });
-  })
-    
-  //API DELETE request 
-  app.delete('/api/notes/:id', (req, res) => {
-      
-    //Read json data file 
-    fs.readFile('../db/db.json', "utf-8", (err, data) => {
-      if (err) throw err;
-      
-      //Parse json data
-      const notes = JSON.parse(data);
+				//Send all notes to client
+        res.json(req.body);
+        console.log("New note saved!");
+			});
+		});
+	});
 
-      //Remove the note with given id 
-      notes.filter(note => {
-        return note.id !== req.param.id
-      })
- 
-      //Write the updated notes to db.json
-      fs.writeFile('../db/db.json', JSON.stringify(notes, null, '\t'), err => {
-        if (err) throw err;
-      })
-    })
-  })
+	//API DELETE request
+	app.delete('/api/notes/:id', (req, res) => {
+		//Read json data file
+		fs.readFile('../db/db.json', 'utf-8', (err, data) => {
+			if (err) throw err;
 
-}
+			//Parse json data
+			const notes = JSON.parse(data);
+
+			//Remove the note with given id
+			notes.filter((note) => {
+				return note.id !== req.param.id;
+			});
+
+			//Write the updated notes to db.json
+			fs.writeFile('../db/db.json', JSON.stringify(notes, null, '\t'), (err) => {
+				if (err) throw err;
+			});
+		});
+	});
+};
