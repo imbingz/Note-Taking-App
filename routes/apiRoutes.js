3096e141a42856290cf9e7cb3ddb0024d84849f1
Bing Z <bing@bings-mbp.lan>
2020-10-07 21:16:06 -0600

    refactored codes.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,49 +3,53 @@
 
 const fs = require('fs');
 
+const db = require('../db/db.json')
 
 //SET UP API ROUTES AND EXPORT THE MODULE ================================================================================
 
-function data() {
-  return JSON.parse(fs.readFileSync("./db/db.json", "utf8"))
-}
+// function data() {
+//   return JSON.parse(fs.readFileSync("./db/db.json", "utf8"))
+// }
 
 module.exports = function(app) {
 
 	//API GET request
   app.get('/api/notes', function(req, res) {
-    res.json(data())
+    //add id to each note when sending response 
+    res.json(db.map((note, i) => ({...note, id: i + ""})))
   })
 
 	//API POST request
   app.post("/api/notes", (req, res) => {
-    let db = data();
+    // let db = data();
     //Set the newNote obj from user input
-    const newNote = {
-      title: req.body.title,
-      text: req.body.text,
-      id: Date.now() + ""
-    }
+    // const newNote = {
+    //   title: req.body.title,
+    //   text: req.body.text,
+    //   id: Date.now() + ""
+    // }
     //add new note to notes data
-    db.push(newNote);
+    db.push(req.body);
   
     //Write the updated notes to db.json
     fs.writeFileSync("./db/db.json", JSON.stringify(db, null, '\t')); 
 
     //Return newNote to user
-    res.json(newNote)
+    res.json(req.body)
   })
 			
 	//API DELETE request
 	app.delete('/api/notes/:id', (req, res) => {
-    let db = data();
+    // let db = data();
     //Remove the note with given id
-    db = db.filter((note) => note.id !== req.params.id);
+    // db = db.filter((note) => note.id !== req.params.id);
+    db.splice(req.params.id, 1)
 
     //Write the updated notes to db.json
     fs.writeFileSync("./db/db.json", JSON.stringify(db, null, '\t'))
 
-    res.json(db)
+    //send response back without body
+    res.status(200).end();
 	});
 };
 
